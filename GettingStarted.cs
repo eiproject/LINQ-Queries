0faeae4dc0ac916e9a29626c4c81b6d698366ab2
Muhammad Razif Rizqullah <razifrizqullah@gmail.com>
2021-08-09 10:14:10 +0700

    add filter using Where  in LINQ and Manual

---
+++ GettingStarted.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ_Queries {
+  class GettingStarted {
+    private string[] _fruits;
+    private IEnumerable<string> _filteredFruit;
+
+    internal GettingStarted(string[] inputArray) {
+      _fruits = inputArray;
+      _filteredFruit = FilterByLengthUsingLINQ(6);
+      WriteResult(_filteredFruit);
+      _filteredFruit = FilterByLengthManual(6);
+      WriteResult(_filteredFruit);
+    }
+
+    private IEnumerable<string> FilterByLengthUsingLINQ(int length) {
+      return _fruits.Where(n => n.Length >= length);
+    }
+
+    private IEnumerable<string> FilterByLengthManual(int length) {
+      return from n in _fruits
+             where n.Length >= length
+             select n;
+    }
+
+    private void WriteResult(dynamic results) {
+      foreach (string result in results) {
+        Console.WriteLine(result);
+      }
+      Console.WriteLine("");
+    }
+  }
+}
--- Program.cs
+++ Program.cs
@@ -7,6 +7,14 @@ using System.Threading.Tasks;
 namespace LINQ_Queries {
   class Program {
     static void Main(string[] args) {
+
+
+      string[] _fruits = new string[] {
+        "apple", "mango", "pinneaple", "melon",
+        "pear", "orange", "banana", "rambutan"
+      };
+      GettingStarted started = new GettingStarted(_fruits);
+      Console.ReadKey();
     }
   }
 }
