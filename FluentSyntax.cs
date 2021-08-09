3bcb5d8f78f253df55cae60191a3441407e39e84
Muhammad Razif Rizqullah <razifrizqullah@gmail.com>
2021-08-09 10:31:59 +0700

    added Fluent Syntax

--- FluentSyntax.cs
+++ FluentSyntax.cs
@@ -6,5 +6,16 @@ using System.Threading.Tasks;
 
 namespace LINQ_Queries {
   class FluentSyntax {
+    private string[] _fruits;
+    internal FluentSyntax(string[] inputArray) {
+      _fruits = inputArray;
+      FilterUsingLinq("a").WriteResult();
+    }
+    private IEnumerable<string> FilterUsingLinq(string word) {
+      return _fruits
+        .Where(n => n.Contains(word))
+        .OrderBy(n => n.Length)
+        .Select(n => n.ToUpper());
+    }
   }
 }
